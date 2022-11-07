4f567ed35fe7b435d645361914c75fbbeaae6151
conpeit0 <111680968+conpeit0@users.noreply.github.com>
2022-11-07 12:16:50 +0900

    Update question.php

--- PHP3-4/question.php
+++ PHP3-4/question.php
@@ -2,15 +2,23 @@
 $name=$_GET['my_name'];//POST送信で送られてきた名前を受け取って変数を作成
 
 //①画像を参考に問題文の選択肢の配列を作成してください。
-
+$opt1=[80,22,20,21];
+$opt2=['PHP','Python','JAVA','HTML'];
+$opt3=['join','select','insert','update'];
 //② ①で作成した、配列から正解の選択肢の変数を作成してください
-
+$ans1=$opt1[0];
+$ans2=$opt2[0];
+$ans3=$opt3[0];
 ?>
-<p>お疲れ様です"$name"さん</p>
+<p>お疲れ様です<?php echo "$name" ; ?>さん</p>
 <!--フォームの作成 通信はPOST通信で-->
 <h2>①ネットワークのポート番号は何番？</h2>
 <!--③ 問題のradioボタンを「foreach」を使って作成する-->
-
+<?php 
+foreach ($opt1 as $value){
+    echo "<input tipe="radio" name="ans1" value=$value />$value" ;
+}?>
+ 
 <h2>②Webページを作成するための言語は？</h2>
 <!--③ 問題のradioボタンを「foreach」を使って作成する-->
 
